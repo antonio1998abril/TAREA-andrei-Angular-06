91f037126a7b5245922d06cd57cdb2c1036bd265
antonio1998abril <ANTONIO.RBARRIENTOS@ALUMNOS.UDG.MX>
2020-03-28 17:43:53 -0600

    1253

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/{{angularandrei06}}'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/angularandrei06'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/{{angularandrei06}}/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/angularandrei06/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// default Heroku PORT
 app.listen(process.env.PORT || 3000);
