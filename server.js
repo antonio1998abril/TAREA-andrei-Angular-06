ee1b5623f37ca39ac06a283e2ae2103bea2f29b1
antonio1998abril <ANTONIO.RBARRIENTOS@ALUMNOS.UDG.MX>
2020-03-28 17:10:17 -0600

    1

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static(__dirname + '/dist/<my-heroes>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<my-heroes>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
